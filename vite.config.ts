31baa2d24b888a8715b01ad98e6c2f2e49760867
dargonc <dargonc@qq.com>
2023-02-27 11:18:19 +0800

    ✨ feat(eslint): 增加eslint ✨ feat(husky): 增加husky ✨ feat(router): 增加基础router

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,47 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import { resolve } from 'path'
+
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir)
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        {
+          // 'naive-ui': [
+          //   'useDialog',
+          //   'useMessage',
+          //   'useNotification',
+          //   'useLoadingBar'
+          // ]
+        }
+      ]
+    }),
+    Components({
+      resolvers: [AntDesignVueResolver()]
+    })
+  ],
+  // 路径别名
+  resolve: {
+    alias: [
+      {
+        find: /\/@\//,
+        replacement: pathResolve('src') + '/'
+      },
+      {
+        find: /\/#\//,
+        replacement: pathResolve('types') + '/'
+      }
+    ]
+  }
 })
